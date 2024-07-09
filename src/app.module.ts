97d8d89486c9a1e67eae1504bb96ce1a815c755d
Simran_Yadav <simranyadav464@gmail.com>
2024-07-10 01:42:09 +0530

    Added the ssl certificate

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,20 +9,28 @@ import { UserModule } from './user/user.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: ['.env'], // Optional: Specify the path to your .env file
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get<string>('DB_HOST'),
         port: configService.get<number>('DB_PORT'),
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
+        ssl: {
+          rejectUnauthorized: true, // Ensure certificates are verified
+          ca: configService.get<string>('SSL_CERTIFICATE_PATH'), // Path to SSL certificate file
+        },
+        extra: {
+          sslmode: 'require',
+        },
         autoLoadEntities: true,
-        synchronize: true,
+        synchronize: true, // Set to true for development, false in production
       }),
-      inject: [ConfigService],
     }),
     AuthModule,
     TodoModule,
