f59789542bab78cd69df32c0644e7fc704bec124
Simran_Yadav <simranyadav464@gmail.com>
2024-07-10 22:39:14 +0530

    Updated the cors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,25 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  // Enable CORS with default settings
-  app.enableCors();
+
+  // List of allowed origins
+  const allowedOrigins = [
+    'https://todo-list-app19.vercel.app',
+    'https://localhost:5000/', // Add more origins as needed
+  ];
+
+  // Enable CORS with specific origins and credentials
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   await app.listen(3000);
 }
